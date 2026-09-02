62f7495692b23e791b13e691ef315b470cc53fad
John75417 <johnvincentpitogo31@gmail.com>
2026-09-02 09:58:26 +0800

    created Activity 2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public static void main (String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("-------------------------------");
     System.out.println("-----PORTFOLIO OF ACTIVITY-----");
-    System.out.println("NAME: DANIEL VILLARINO");
-    System.out.println("SECTION AND YEAR: IT2C");
+    System.out.println("NAME:JOHN VINCENT O. PITOGO");
+    System.out.println("SECTION AND YEAR: IT2A");
     System.out.println("------CONTENTS-----------------");
     System.out.println("1. Activity 1");
     System.out.println("2. Activity 2");
